36f0047052d229f8b1f6e24566ac766f903e3bb9
Deon <deonlcox@gmail.com>
2018-07-22 00:14:41 +0900

    Add files via upload
    login form

---
+++ slumper.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>refresher</title>
+    <script type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="slumper.css">
+
+    
+  </head>
+  <body>
+    <div id="container">
+      <div class="heading">
+        
+
+         <!--navbar-->
+        
+        <section>
+            <ul class="sidenav">
+             <li><a class="active" href="#home">SLUMPER</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            </ul>
+</section>
+      </div>
+    </div>
+
+    <div class="body">
+      
+    </div>
+   <div id="bg">
+  <!--new registration-->
+  <div id= "registration">
+      <h1>Register</h1>
+   <p>Name: <input type="text" name="user"  placeholder="username"> </p> 
+   <p>Password: <input type="text" name="pass"  placeholder="password"> </p>
+   <p>Confirm password: <input type="text" name="pass2"  placeholder="confirm"> </p>
+   <p>Email: <input type="text" name="email"  placeholder="email"> </p>
+   <p>Confirm email: <input type= 'text' name="email2" placeholder="confirm"</p>
+    <P><button type="button" name="button" value="submit" ><b>Submit</b> </button></P>
+</div>
+</div>
+    
+   
+  </body>
+</html>
